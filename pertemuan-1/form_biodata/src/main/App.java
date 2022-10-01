c4bf9303a9f5034615a2c8fcf866bbe8820b85c1
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2022-10-01 21:48:53 +0700

    add: form_biodata (09/10/2022)

---
+++ pertemuan-1/form_biodata/src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.Form;
+
+public class App {
+    
+    public static void main(String[] args){
+        Form panel = new Form();
+        panel.setVisible(true);
+    }
+    
+}
