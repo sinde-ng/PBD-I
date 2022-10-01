66be4443528198f1eba2abdfbf4fcc90c3b9993c
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2022-10-01 21:59:24 +0700

    add: simple_sum

---
+++ pertemuan-1/penjumlahan_sederhana/src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.Logic;
+
+public class App {
+    
+    public static void main(String[] args){
+        Logic panel = new Logic();
+        panel.setVisible(true);
+    }
+    
+}
